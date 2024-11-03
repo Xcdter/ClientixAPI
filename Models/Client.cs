9e7c7c6772397b7b8d467dfc88982aad0ec2c824
Xcdter <tigrasha999@ya.ru>
2024-11-03 03:54:59 +0300

    +Models

---
+++ Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ClientixAPI.Models
+{
+    public class Client
+    {
+        public Guid Id { get; set; }
+        public string INN { get; set; }
+        public string Name { get; set; }
+        public ClientType Type { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Founder> Founders { get; set; }
+    }
+
+    public enum ClientType
+    {
+        LegalEntity,
+        IndividualEntrepreneur
+    }
+
+}
---
+++ Models/Founder.cs
@@ -0,0 +1,13 @@
+﻿namespace ClientixAPI.Models
+{
+    public class Founder
+    {
+        public Guid Id { get; set; }
+        public string INN { get; set; }
+        public string FullName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public Guid ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
